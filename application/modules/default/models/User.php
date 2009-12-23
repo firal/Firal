375b7f3663bb93faeaecc22db2cd593222ffd10f
Kokx <pieter@kokx.nl>
2009-12-23 02:35:06 +0100

    User model added

--- application/modules/default/models/User.php
+++ application/modules/default/models/User.php
@@ -26,5 +26,135 @@
  * @copyright  Copyright (c) 2009 Firal (http://firal.org/)
  * @license    http://firal.org/licenses/new-bsd    New BSD License
  */
-class Default_Model_User extends Firal_Model_ModelAbstract
-{}
+class Default_Model_User extends Firal_Model_ModelAbstract implements Zend_Acl_Role_Interface
+{
+
+    /**
+     * Id
+     *
+     * @var int
+     */
+    protected $_id;
+
+    /**
+     * Name
+     *
+     * @var string
+     */
+    protected $_name;
+
+    /**
+     * Password
+     *
+     * @var string
+     */
+    protected $_password;
+
+    /**
+     * Email
+     *
+     * @var string
+     */
+    protected $_email;
+
+    /**
+     * Role
+     *
+     * @var string
+     */
+    protected $_role;
+
+
+    /**
+     * Get the role id
+     *
+     * @return string
+     */
+    public function getRoleId()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Get the id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Get the username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Set the password
+     *
+     * @param string $password
+     *
+     * @return Default_Model_User
+     */
+    public function setPassword($password)
+    {
+        $this->_password = sha1($password);
+
+        return $this;
+    }
+
+    /**
+     * Get the email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * Set the email
+     *
+     * @param string $email
+     *
+     * @return Default_Model_User
+     */
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->_role;
+    }
+
+    /**
+     * Set the role
+     *
+     * @param string $role
+     *
+     * @return Default_Model_User
+     */
+    public function setRole($role)
+    {
+        $this->_role = $role;
+
+        return $this;
+    }
+
+}
