6633666879108d3b02a10d2612b97035e330d472
Kokx <pieter@kokx.nl>
2010-03-13 23:50:03 +0100

    Created basic DI container for the pages module.

---
+++ application/modules/pages/DiContainer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Firal
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://firal.org/licenses/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to firal-dev@googlegroups.com so we can send you a copy immediately.
+ *
+ * @category   Firal
+ * @package    Firal_Di
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+
+
+/**
+ * Dependency Injection container for the Pages module
+ *
+ * @category   Firal
+ * @package    Firal_Di
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+class Pages_DiContainer extends Firal_Di_Container_ContainerAbstract
+{
+
+}
