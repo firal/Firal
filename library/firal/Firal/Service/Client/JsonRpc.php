e6793bf9d0ea06f2460527416ca637cb722291a6
Kokx <pieter@kokx.nl>
2010-04-15 19:37:35 +0200

    Renamed the Json client to JsonRpc client

--- library/firal/Firal/Service/Client/JsonRpc.php
+++ library/firal/Firal/Service/Client/JsonRpc.php
@@ -28,7 +28,7 @@
  * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
  * @license    http://firal.org/licenses/new-bsd    New BSD License
  */
-class Firal_Service_Client_Json implements Firal_Service_Client_ClientInterface
+class Firal_Service_Client_JsonRpc implements Firal_Service_Client_ClientInterface
 {
 
     /**
