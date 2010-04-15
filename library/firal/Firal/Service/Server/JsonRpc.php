a7d4bd1aa429f4deee3ec36f43645508861ebb36
Evan Coury <me@evancoury.com>
2010-04-15 09:26:29 -0700

    added xml-rpc and json-rpc servers

---
+++ library/firal/Firal/Service/Server/JsonRpc.php
@@ -0,0 +1,34 @@
+ <?php
+/**
+ * Firal
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://firal.org/licenses/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to firal-dev@googlegroups.com so we can send you a copy immediately.
+ *
+ * @category   Firal
+ * @package    Firal_Service
+ * @subpackage Server
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+
+/**
+ * JSON-RPC server
+ *
+ * @category   Firal
+ * @package    Firal_Service
+ * @subpackage Server
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+class Firal_Service_Server_JsonRpc extends Zend_Json_Server implements Firal_Service_Server_ServerInterface
+{
+
+}
