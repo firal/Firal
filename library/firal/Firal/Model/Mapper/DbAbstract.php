4fdb5efa4f00344bdc05fd67a8416256722edb55
Kokx <pieter@kokx.nl>
2010-04-15 17:10:44 +0200

    Renamed the abstract mapper and re-added the old mapper as db mapper

---
+++ library/firal/Firal/Model/Mapper/DbAbstract.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Firal
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://firal.org/licenses/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to firal-dev@googlegroups.com so we can send you a copy immediately.
+ *
+ * @category   Firal
+ * @package    Firal_Model
+ * @subpackage Mapper
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+
+/**
+ * Abstract model mapper
+ *
+ * @category   Firal
+ * @package    Firal_Model
+ * @subpackage Mapper
+ * @copyright  Copyright (c) 2009-2010 Firal (http://firal.org/)
+ * @license    http://firal.org/licenses/new-bsd    New BSD License
+ */
+abstract class Firal_Model_Mapper_MapperAbstract
+{
+
+    /**
+     * Database adapter
+     *
+     * @var Zend_Db_Adapter_Abstract
+     */
+    protected $_adapter;
+
+    /**
+     * Default database adapter
+     *
+     * @var Zend_Db_Adapter_Abstract
+     */
+    protected static $_defaultAdapter;
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $_name;
+
+
+    /**
+     * Constructor
+     *
+     * This method is final, because it should not be used to initialize, use
+     * {@link _init()} instead
+     *
+     * @param Zend_Db_Adapter_Abstract $adapter
+     *
+     * @throws Firal_Model_Mapper_RuntimeException If there is no adapter defined
+     *
+     * @return void
+     */
+    final public function __construct(Zend_Db_Adapter_Abstract $adapter = null)
+    {
+        if (null === $adapter) {
+            if (null === ($adapter = self::getDefaultAdapter())) {
+                throw new Firal_Model_Mapper_RuntimeException('There was no adapter defined');
+            }
+        }
+
+        $this->_adapter = $adapter;
+
+        $this->_init();
+    }
+
+    /**
+     * Init hook
+     *
+     * @return void
+     */
+    protected function _init()
+    {}
+
+    /**
+     * Get the database adapter
+     *
+     * @return Zend_Db_Adapter_Abstract
+     */
+    public function getAdapter()
+    {
+        return $this->_adapter;
+    }
+
+    /**
+     * Get the table name
+     *
+     * @return string
+     */
+    public function getTableName()
+    {
+        return $this->_name;
+    }
+
+    // static functions
+
+    /**
+     * Set the default database adapter
+     *
+     * @param Zend_Db_Adapter_Abstract $adapter
+     *
+     * @return void
+     */
+    public static function setDefaultAdapter(Zend_Db_Adapter_Abstract $adapter)
+    {
+        self::$_defaultAdapter = $adapter;
+    }
+
+    /**
+     * Get the default database adapter
+     *
+     * @return Zend_Db_Adapter_Abstract
+     */
+    public static function getDefaultAdapter()
+    {
+        return self::$_defaultAdapter;
+    }
+
+}
